1a3ceaaaabeaf02b622391e29aae86de8172f0ea
totm12 <115745369+totm12@users.noreply.github.com>
2022-10-19 19:38:41 -0400

    Implemented query/count and merge functions

--- src/dams_qgis.py
+++ src/dams_qgis.py
@@ -2,13 +2,13 @@ import processing
 import time
 
 def reproject_crs(lyr1, change_crs):
-    #Reproject layers.
+    #Returns reprojected geometry.
     #Parameters:
     #lyr1 : shapefile//path
     #The name/path of the layer to be reprojected
     #change_crs : string of the new CRS
     #(Uses temp layers to reduce memory consumption)
-    print(lyr1,change_crs)
+    #print(lyr1,change_crs)
     rp = processing.run('native:reprojectlayer',
     {'INPUT': lyr1,
     'TARGET_CRS': change_crs,
@@ -17,12 +17,12 @@ def reproject_crs(lyr1, change_crs):
     return rp
 
 def clipping(lyr1, lyr2):
-    #Run clip tool.
+    #Returns geometry clipped to overlay.
     #Parameters:
     #lyr1 : shapefile//path
     #The name of the input shapefile to be clipped
     #lyr2 : path to vector data
-    #The name of the overlay area
+    #The name of the overlay area to clip against
     layer_clip = processing.run('qgis:clip',
         {'INPUT': lyr1,
         'OVERLAY': lyr2,
@@ -31,7 +31,7 @@ def clipping(lyr1, lyr2):
     return layer_clip
 
 def buff_points(static_dis, lyr):
-    #Run buffer tool on clipped points.
+    #Returns buffered point geometry.
     #Parameters:
     #static_dis : shapefile//path
     #The value used to buffer by (distance)
@@ -46,6 +46,40 @@ def buff_points(static_dis, lyr):
     )
     return buff
 
+def query_count(lyr1, lyr2):
+    #Returns count of selected features within and
+    #updated layer with duplicates deleted.
+    #Parameters:
+    #static_dis : shapefile//path
+    #The value used to buffer by (distance)
+    #lyr : shapefile//path
+    #The name of the input shapefile to buffer
+    #lyr2 : shapefile//path
+    #The name of the output path
+    local_s = processing.run('qgis:selectbylocation',
+    {'INPUT': lyr1,
+    'PREDICATE': [6],
+    'INTERSECT': lyr2,
+    'METHOD':0}
+    )
+    s_feature = lyr1.selectedFeatureCount()
+    lyr1.startEditing()
+    lyr1.deleteSelectedFeatures()
+    lyr1.deleteAttributes([9])
+    lyr1.commitChanges()
+    return s_feature, lyr1
+
+def layer_merge(lyr_list):
+    #Returns merged vector geometry from list.
+    #Parameters:
+    #lyr_list : []
+    #List of layers to be merged
+    merge_on = processing.run('native:mergevectorlayers',
+    {'LAYERS': lyr_list,
+    'OUTPUT':'memory:Deduplipcated' }
+    )
+    return merge_on
+    
 start_time = time.time()
 #Set input and output file paths
 on_dam_path = r"M:\School\4YP3\DS\Data\Ontario_Dam_Inventory\Ontario_Dam_Inventory.shp"
@@ -66,7 +100,6 @@ clipped_geodar_res = clipping(geodar_res_path,basin_clip_path)
 #QgsProject.instance().addMapLayer(clipped_geodar_dams['OUTPUT'])
 #QgsProject.instance().addMapLayer(clipped_geodar_res['OUTPUT'])
 
-
 #reproject layers to match regional CRS
 repro_dams_on = reproject_crs(clipped_dams_on['OUTPUT'],'EPSG:32618')
 repro_hydro_rivers = reproject_crs(clipped_hydro_rivers['OUTPUT'],'EPSG:32618')
@@ -82,27 +115,13 @@ repro_geodar_res = reproject_crs(clipped_geodar_res['OUTPUT'],'EPSG:32618')
 buff_geodar_dams = buff_points(2000,repro_geodar_dam['OUTPUT'])
 QgsProject.instance().addMapLayer(buff_geodar_dams['OUTPUT'])
 QgsProject.instance().addMapLayer(repro_dams_on['OUTPUT'])
+
 #location query
-processing.run('qgis:selectbylocation',
-    {'INPUT': repro_dams_on['OUTPUT'],
-    'PREDICATE': [6],
-    'INTERSECT': buff_geodar_dams['OUTPUT'],
-    'METHOD':0}
-    )
-print('Count:',
-repro_dams_on['OUTPUT'].selectedFeatureCount())
-#delete duplicates 
-repro_dams_on['OUTPUT'].startEditing()
-repro_dams_on['OUTPUT'].deleteSelectedFeatures()
-repro_dams_on['OUTPUT'].deleteAttributes([9])
-repro_dams_on['OUTPUT'].commitChanges()
+on_count,repro_dams_on['OUTPUT'] = query_count(repro_dams_on['OUTPUT'],buff_geodar_dams['OUTPUT'])
+
 #merge old with new
 shapefile_list = [repro_dams_on['OUTPUT'],repro_geodar_dam['OUTPUT']]
-print(shapefile_list)
-
-merge_on = processing.run('native:mergevectorlayers',
-    {'LAYERS': shapefile_list,
-    'OUTPUT':'memory:Deduplipcated' }
-    )
-QgsProject.instance().addMapLayer(merge_on['OUTPUT'])
+merged_data = layer_merge(shapefile_list)
+QgsProject.instance().addMapLayer(merged_data['OUTPUT'])
+print('Count:',on_count,'duplicate features within a 2km buffer.')
 print("%s seconds" % (time.time() - start_time))
